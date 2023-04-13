3afa20d2004bc7f5944add83e67c9ebff339a401
Joris <5628733-Joristdh@users.noreply.gitlab.com>
2023-04-13 16:05:37 +0200

    init

---
+++ examples/rpl-border-router/build/cc26x0-cc13x0/sensortag/cc2650/obj/.deps/syscalls.d
@@ -0,0 +1,5 @@
+build/cc26x0-cc13x0/sensortag/cc2650/obj/syscalls.o: \
+ ../../os/lib/newlib/syscalls.c \
+ ../../os/services/rpl-border-router/module-macros.h
+
+../../os/services/rpl-border-router/module-macros.h:
